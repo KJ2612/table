5408bdee6734f6f9d576b95619c966198e19c117
root <root@localhost>
2021-10-07 14:02:07 +0530

    auto publish 1633595527456

---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+print("      MY MULTIPLICATION TABLE TELLER. ")
+
+num = int(input("Enter a number: "))
+
+count = 1
+
+while count <= 10:
+  multply = num * count
+  print(f"{num} x {count} = {multply}")
+  count = count + 1
